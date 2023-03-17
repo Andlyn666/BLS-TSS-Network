6aeb216805d19808039a68b1d5b472c41b72f940
Andlyn <lyn.sdfz@163.com>
2023-03-17 14:24:59 +0800

    Add retry time parameter  to the log keyword.

--- tests/scenarios/src/environment/log.py
+++ tests/scenarios/src/environment/log.py
@@ -24,7 +24,7 @@ def get_log_info(log, keyword):
     return None
 
 
-def get_keyword_from_log(process, keyword):
+def get_keyword_from_log(process, keyword, retry_time=30):
     """
     Run a command as a sub-process and check the logs for a given keyword
 
@@ -32,13 +32,13 @@ def get_keyword_from_log(process, keyword):
     :param keyword: keyword to look for in the log
     :return: dictionary with the relevant information found for the keyword
     """
-    max_retry = 30
-    for i in range(max_retry):
+    max_retry = retry_time
+    i =  0
+    while (i < max_retry):
+        i = i + 1
         log_info = get_log_info(iter(process.stdout.readline, b''), keyword)
-
         if log_info is not None:
             return log_info
-
         time.sleep(1)
     return 'Can not find the keyword in log within limit time.'
 
